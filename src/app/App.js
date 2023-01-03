15806256d1df8be623131e2c79fa5bb8dd1ca3df
Mustafa <qralcxx61@outlook.com>
2023-01-03 09:11:18 +0200

    initial

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import logo from '../logo.svg';
+import './App.css';
+import classes from "./App.css"
+import Shell from "./shell/Shell";
+
+function App() {
+  return (
+    <>
+      <Shell/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/app/shell/view/layout/tabContent/TabContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./TabContent.module.css"
+
+
+const TabContent = props => {
+    return <div className={classes.TabContent}>
+        <div className={classes.TabBody}>
+
+        </div>
+
+    </div>
+
+}
+
+export default TabContent;
